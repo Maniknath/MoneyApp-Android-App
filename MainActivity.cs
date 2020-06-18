ceb017bcbe00f574012fcd51ffe17b4bb75ea5e7
maniknath <maniknath79@gmail.co>
2020-06-18 16:01:00 -0400

    fifth android app

---
+++ MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Calligraphy;
+
+namespace MoneyApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //Initializing calligraphy, build font atribute.
+            CalligraphyConfig.InitDefault(new CalligraphyConfig.Builder()
+                .SetDefaultFontPath("font/GlacialIndifference-Regular.ttf")
+                .SetFontAttrId(Resource.Attribute.fontPath)
+                .Build());
+
+            SetContentView(Resource.Layout.activity_main);
+
+            SupportActionBar.Title = "MONEY APP";
+        }
+
+        protected override void AttachBaseContext(Context @base)
+        {
+            base.AttachBaseContext(CalligraphyContextWrapper.Wrap(@base));
+        }
+
+
+    }
+}
